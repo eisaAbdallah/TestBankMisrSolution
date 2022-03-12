d6a701b7d82110cfb2ad6b35fa26b86a77ce2087
eisaAbdallah <89872569+eisaAbdallah@users.noreply.github.com>
2022-03-12 18:42:32 +0200

    Update CreateConfigurationPlot.sql

--- src/main/resources/CreateConfigurationPlot.sql
+++ src/main/resources/CreateConfigurationPlot.sql
@@ -1,8 +1,9 @@
 DROP TABLE `irrigation_period_details` IF EXISTS;
 
 CREATE TABLE `irrigation_period_details` (
-  `id` bigint NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `water_amount` int DEFAULT NULL,
   `time_slot` time DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
