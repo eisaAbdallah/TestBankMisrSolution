f81137657c224c37c9a9fcca793c8ac511ec3bef
eisaAbdallah <89872569+eisaAbdallah@users.noreply.github.com>
2022-03-13 09:17:00 +0200

    Update ApiIrrigationResource.java

--- src/main/java/com/example/bankmisr/Test/api/ApiIrrigationResource.java
+++ src/main/java/com/example/bankmisr/Test/api/ApiIrrigationResource.java
@@ -106,7 +106,7 @@ public class ApiIrrigationResource {
         headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
 
          try {
-         ResponseEntity<PlotData[]> response=this.restTemplate.getForEntity("https://domainofsensordevice.com/url",PlotData[].class);
+         ResponseEntity<PlotData[]> response=this.restTemplate.getForEntity("https://domainofsensordevice.com",PlotData[].class);
 
          } catch (HttpStatusCodeException ex) {
              counterOfRetryTimes++;
